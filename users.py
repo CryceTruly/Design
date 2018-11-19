530c7437978e5ec001489d01e3f9a0e20631bc6b
cryce truly <crycetruly@gmail.com>
2018-11-19 11:46:29 +0300

    Updated users.py
    Added comments and refactored getname

--- users.py
+++ users.py
@@ -1,11 +1,16 @@
 import datetime
-
+'''
+the user class describes user properties and behaviours
+'''
 class user(object):
   def __init__(self, name):
     self.name = name
   
   def get_name(self, value = None):
-    if(not value):
+    '''
+    returns a user name passed in into the class
+    '''
+    if not value:
       return(self.name)
     else:
       self.name = value
