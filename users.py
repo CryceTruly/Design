30b168d307bd4d18f91c3f3aa4db8d7c77e292cf
Ivankivu <ivankivu@gmail.com>
2018-11-19 11:39:28 +0300

    Modified user.py to add  user login and logout functionality
    [delivers #162044247]

--- users.py
+++ users.py
@@ -1,39 +1,38 @@
 import datetime
 
-class user(object):
-  """
-  class 
-  """
-  def __init__(self, name):
-    self.name = name
-    self._logged_in = False
-    self.last_logged_in_at = None
-  
-  def get_name(self, value = None):
-    """
-    This method handles setting the name
-    """
-    if(not value):
-      return(self.name)
-    else:
-      self.name = value
 
-  def is_logged_in(self):
-    return(self._logged_in)
-  
-  def get_last_logged_in_at(self):
-    """
-    This function returns the user last login status
-    """
-    return(self.last_logged_in_at)
-  
-  def log_in(self):
-     """
-    This function returns the user last login time record
-    """
-    self._logged_in = True
-    self.last_logged_in_at = datetime.datetime.now()
-  
-  def log_out(self):
-    
-    self._logged_in = False
+class user(object):
+
+    def __init__(self, name):
+        self.name = name
+        self._logged_in = False
+        self.last_logged_in_at = None
+
+    def get_name(self, value=None):
+        """
+        This method handles setting the name
+        """
+        if(not value):
+            return(self.name)
+        else:
+            self.name = value
+
+    def is_logged_in(self):
+        return(self._logged_in)
+
+    def get_last_logged_in_at(self):
+        """
+        This function returns the user last login status
+        """
+        return(self.last_logged_in_at)
+
+    def log_in(self):
+        """
+        This function returns the user last login time record
+        """
+        self._logged_in = True
+        self.last_logged_in_at = datetime.datetime.now()
+
+    def log_out(self):
+
+        self._logged_in = False
