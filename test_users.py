41b112a52431b63d87e10597532819b842f27ef6
crycetruly <crycetruly@gmail.com>
2018-11-19 12:25:24 +0300

    resolved conflicts

--- test_users.py
+++ test_users.py
@@ -3,14 +3,6 @@ from users import user
 
 user1 = user('User 1')
 
-<<<<<<< Updated upstream
-class Test(unittest.TestCase):
-  def test_instantiation(self):
-    self.assertEqual(user1.name,'User 1', 'User name is set cor')
-
-if __name__ == '__main__':
-    unittest.main()
-=======
 
 class Test(unittest.TestCase):
     def test_instantiation(self):
@@ -18,4 +10,4 @@ class Test(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
->>>>>>> Stashed changes
+
