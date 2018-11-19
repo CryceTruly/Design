ed34335dfc250297ee8f4c5df842cf12a71eafed
Ivankivu <ivankivu@gmail.com>
2018-11-19 11:45:37 +0300

    Modified user.py to add  user last login time
    [delivers #162044691]

--- users.py
+++ users.py
@@ -9,3 +9,32 @@ class user(object):
       return(self.name)
     else:
       self.name = value
+import datetime
+
+
+class user(object):
+
+    def __init__(self, name):
+        self.name = name
+        self._logged_in = False
+        self.last_logged_in_at = None
+
+    def get_name(self, value=None):
+        if(not value):
+            return(self.name)
+        else:
+            self.name = value
+
+    def is_logged_in(self):
+        return(self._logged_in)
+
+    def get_last_logged_in_at(self):
+        return(self.last_logged_in_at)
+
+    def log_in(self):
+        self._logged_in = True
+        self.last_logged_in_at = datetime.datetime.now()
+
+user1 = user("Stanley")
+user1.log_in()
+print(user1.last_logged_in_at)
